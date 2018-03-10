bc47b72fa994363ba8005a43b9be2ba640fadabc
James Muoghalu <jmuoghalu00@outlook.com>
2018-03-10 14:33:12 -0600

    Document Processing Update
    still need to remove whitespace and error tags

--- source/docproc.py
+++ source/docproc.py
@@ -1,27 +1,36 @@
 import math, os, sys
-from HTMLParser import HTMLParser
+from html.parser import HTMLParser
+from html.entities import name2codepoint
 
 class DocProcessor(HTMLParser):
 
     def __init__(self):
-        HTMLParser.__init__(self)
-        self.in_body = False
+        super().__init__()
+        self.reset()
         self.out_file = None
+        self.in_script = self.in_cite = self.in_span = False
+        self.convert_charrefs = True
 
     def handle_starttag(self, tag, attrs):
-        if tag == "body":
-            self.in_body = True
-
-        self.out_file.write("<" + tag)
-        for x in attrs:
-            self.out_file.write(" " + str(x[0]) + "=\"" + str(x[1]) + "\"")
-        self.out_file.write(">")
+        if tag == "script":
+            self.in_script = True
+        elif tag == "span":
+            self.in_span = True
+        elif tag == "cite":
+            self.in_cite = True
 
     def handle_endtag(self, tag):
-        self.out_file.write("</" + tag + ">")
+        if tag == "script":
+            self.in_script = False
+        elif tag == "span":
+            self.in_span = False
+        elif tag == "cite":
+            self.in_cite = False
 
     def handle_data(self, data):
-        self.out_file.write(data)
+        if (not self.in_script) and (not self.in_cite) and (not self.in_span):
+            self.out_file.write(data.lower())
+
 
 
 
@@ -33,20 +42,18 @@ if __name__ == '__main__':
 
         out_name = ""
         for i in range(11, len(in_file_name)):
+            if in_file_name[i] == '.':
+                break
             out_name += in_file_name[i]
-        dp.out_file = open("../processed_docsnew/" + out_name, "w")
+        dp.out_file = open("../processed_docsnew/" + out_name + ".txt", "w")
         dp.out_file.write("<!DOCTYPE html>")
 
         # the docsnew files have each tag written in one line
         read_from_file = test_file.readline()
         while(read_from_file):
-            if read_from_file[len(read_from_file)-1] == '\n':
-                dp.feed(read_from_file)
-                read_from_file = test_file.readline()
-            else:
-                dp.feed(read_from_file)
-                print "End of File\n"
-                break
+            dp.feed(read_from_file)
+            read_from_file = test_file.readline()
+
 
     except Exception as e:
         raise
