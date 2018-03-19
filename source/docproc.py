ad34727aa164da8bede710db783b01670d6bf37d
James Muoghalu <jmuoghalu00@outlook.com>
2018-03-18 21:26:25 -0500

    Implemented Stop List and Stemmer

--- source/docproc.py
+++ source/docproc.py
@@ -1,6 +1,8 @@
-import math, os, sys
+import math, os, re, sys
 from html.parser import HTMLParser
 from html.entities import name2codepoint
+from nltk.stem.porter import *
+from nltk.corpus import stopwords
 
 class DocProcessor(HTMLParser):
 
@@ -10,6 +12,7 @@ class DocProcessor(HTMLParser):
         self.out_file = None
         self.in_script = self.in_cite = self.in_span = False
         self.convert_charrefs = True
+        self.stemmer = PorterStemmer()
 
     def handle_starttag(self, tag, attrs):
         if tag == "script":
@@ -29,7 +32,14 @@ class DocProcessor(HTMLParser):
 
     def handle_data(self, data):
         if (not self.in_script) and (not self.in_cite) and (not self.in_span):
-            self.out_file.write(data.lower().strip() + " ")
+            data_list = (re.sub(r"[^a-zA-Z0-9_ ]+", "", data.lower().strip())).split()
+                # re.sub = remove non alphanumeric characters from the string; NOTE: this alters the format of hyperlinks
+            rm_dl_stopwords = [dl for dl in data_list if dl not in stopwords.words("english")]
+            stemmed_list = [self.stemmer.stem(dl) for dl in rm_dl_stopwords]
+
+            if len(stemmed_list) > 0:
+                for sl in stemmed_list:
+                    self.out_file.write(sl + " ")
 
 
 if __name__ == '__main__':
@@ -53,7 +63,6 @@ if __name__ == '__main__':
                     while(read_from_file):
                         dp.feed(read_from_file)
                         read_from_file = curr_file.readline() # the docsnew files have each tag written in one line
-
 
     except Exception as e:
         raise
