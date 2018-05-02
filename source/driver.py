30e606dc79e1b2cdfde8490e2d60055bd12cf751
James Muoghalu <jmuoghalu00@outlook.com>
2018-05-01 23:36:23 -0500

    driver.py Is Now Main Project File; Now Writing Inverted Index to File
    Created 'data' Folder to Hold Inverted Indexes for Various Document Source Folders

---
+++ source/driver.py
@@ -0,0 +1,97 @@
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from re import sub as re_sub
+from indexer import InvertedIndex as InvertedIndexClass
+from vsm import VectorSpaceModel as VSMClass
+from query import Query as QueryClass
+
+def debugPrint(query: QueryClass, vsm: VSMClass, iic: InvertedIndexClass):
+
+    """
+    print("\n\nInverted Index:")
+    for term, index in iic.inverted_index.items():
+        print(end="" "{ %s , %s , %s } " % (index.term, len(index.docID_list), index.term_tf) )
+        first = True
+        if len(index.term) < 4:
+            print(end="" "\t")
+        print(end="" "\t\t -> ")
+
+        for docID, TF in index.posting_list.items():
+            if not first:
+                print(end="" " -> ")
+            print(end="" "[%s | %s]" % (docID, TF))
+            first = False
+        print("")
+
+    print("\n\nVector Space Model")
+    print("term\t\tIDF\t\t", end='')
+    for x in iic.document_list:
+        print(x[0] + "\t\t", end='')
+    print()
+
+    for term, IDF in vsm.terms_idf.items():
+        if len(term) < 8:
+            print(term + "\t\t", end='')
+        else:
+            print(term + "\t", end='')
+        print(str(IDF) + "\t\t", end='')
+
+        for weight in vsm.terms_weights[term]:
+            print(str(weight) + "\t\t", end='')
+
+        print()
+
+    print("\n\nDocument Lengths")
+    for i in range(0, len(vsm.document_vectors)):
+        doc = vsm.document_vectors[i]
+        print(end='' "\t[ %s" % (str(doc[0])))
+        for j in range(1,len(doc)):
+            print(end='' ", %s" % (str(doc[j])))
+        print("] == |%s|" % (vsm.document_lengths[i][1])) #
+        print("\t[...] == |%s|" % (vsm.document_lengths[i][1]))
+    #"""
+
+    #"""
+    print("\n\nQuery")
+    print("Terms: %s" % (query.query_vector.keys()))
+    print(end='' "Result: [D%s, %s]" %
+        (query.all_similarities[0][0], query.all_similarities[0][1]))
+    for i in range(1, len(query.all_similarities)):
+        print(end='' ", [D%s, %s]" %
+            (query.all_similarities[i][0], query.all_similarities[i][1]))
+    #"""
+    print("\n\n")
+
+
+if __name__ == "__main__":
+    try:
+        base_doc_name = "testdoc" # the actual name of the folder containing the processed files
+        proc_doc_location = "../" + base_doc_name
+
+        iic = InvertedIndexClass()
+        iic.createInvertedIndex(proc_doc_location)
+        vsm = VSMClass(iic)
+        stemmer = PorterStemmer()
+
+        query1 = ["silver", "truck"]
+        query2 = ["damaged", "truck"]
+        query3 = ["acadia"]
+        queries = [query1, query2, query3]
+        for query in queries:
+            for i in range(0, len(query)):
+                #item = (re_sub(r"[^a-zA-Z0-9_ ]+", "", item.lower().strip())).split()
+                if query[i] not in stopwords.words("english"):
+                    query[i] = stemmer.stem(query[i])
+            qr = QueryClass(query, vsm)
+            debugPrint(qr, vsm, iic)
+            break
+
+        """
+            # TODO:
+                1.) terminal I/O for user strings
+                2.) create runQuery function which will pass the user's strings, run them through the stop list and stemmer, and use them to create the QueryClass object
+                3.) take the sorted similarity list, retrieve the document ID's, use the ID's to get the document basenames, and use the basenames to retrieve pages from the document source folder (NOT THE PROCESSED FOLDER) 
+        """
+
+    except Exception as e:
+        raise()
--- source/indexer.py
+++ source/indexer.py
@@ -1,10 +1,10 @@
-import os
+import math, os
 from collections import OrderedDict
 from re import sub as re_sub
 
 class InvertedIndex:
     def __init__(self):
-        self.inverted_index = {}
+        self.inverted_index = {} # {term: IndexEntry}
         self.document_list = [] # [[docName, docID]] # using a list instead of a dictionary to preserve ordering by docID
 
     class IndexEntry:
@@ -13,13 +13,13 @@ class InvertedIndex:
             self.term_idf = 0
             self.docID_list = [] # the size of this list is the term's DF
             self.term_tf = 0
-            self.posting_list = {}
+            self.posting_list = {} # [docID: docTF]
 
 
     def createInvertedIndex(self, proc_doc_location):
         try:
-            #proc_doc_location = "../processed_testdoc/"
-            #proc_doc_location = "../testdoc/" # NOTE: files in this directory contain punctuation
+            self.inverted_index = {}
+            self.document_list = []
             current_docID = 1
 
             if not proc_doc_location[len(proc_doc_location)-1] == '/':
@@ -27,7 +27,7 @@ class InvertedIndex:
 
             if not os.path.exists(proc_doc_location):
                 print("The Input Directory Does Not Exist")
-                return {}
+                return
 
             """
             build the unsorted inverted index
@@ -37,7 +37,10 @@ class InvertedIndex:
             """
             for root, dirs, files in os.walk(proc_doc_location):
                 for f in files:
-                    self.document_list.append([(os.path.splitext(f)[0]), current_docID])
+                    # drops the file extension
+                    #self.document_list.append([(os.path.splitext(f)[0]), current_docID])
+                    # does not drop the file extension
+                    self.document_list.append([f, current_docID])
                     curr_file = open((proc_doc_location + f), "r", encoding="UTF8")
 
                     # read a line; convert to lowercase; remove punctuation, and separate the words
@@ -62,7 +65,13 @@ class InvertedIndex:
                         read_from_file = (re_sub(r"[^a-zA-Z0-9_ ]+", "", curr_file.read().lower())).split()
                     current_docID += 1
 
+            # set all term IDF's
+            for entry in self.inverted_index.values():
+                entry.term_idf = float("{0:.3f}".format(math.log10( len(self.document_list) / len(entry.docID_list) )))
+
             self.inverted_index = OrderedDict(sorted(self.inverted_index.items(), key=lambda t: t[0])) # sort the inverted index
+
+            iid_file_name = "../data/" + os.path.basename(os.path.dirname(proc_doc_location)) + "_index.txt"
 
             """ OUTPUT FOR DEBUGGING
             for term, index in self.inverted_index.items():
@@ -79,6 +88,41 @@ class InvertedIndex:
                     first = False
                 print("")
             #"""
+            iid_file = open(iid_file_name, "w", encoding="UTF8")
+            for docInfo in self.document_list: # [[docName, docID]]:
+                iid_file.write("D%s:\t\t%s\n" % (docInfo[1], docInfo[0]))
+
+            iid_file.write("\n\n\n")
+            for term, entry in self.inverted_index.items():
+                iid_file.write("Term:\t%s\n" % term)
+                iid_file.write("\tDF:\t%s\n" % len(entry.docID_list))
+                iid_file.write("\tTF:\t%s\n" % entry.term_tf)
+                iid_file.write("\tIDF:\t%s\n" % entry.term_idf)
+                #iid_file.write()
+                iid_file.write("\tPosting List: (first line = docID; second line = docTF)\n")
+
+                docID_line = ""
+                postlist_line = ""
+                for docID, TF in entry.posting_list.items():
+                    docID_line += "{0}\t\t".format(docID)
+                    postlist_line += "{0}\t\t".format(TF)
+                iid_file.write("\t\t{0}\n\n\t\t{1}\n".format(docID_line, postlist_line))
+                iid_file.write("\n\n")
+
+            iid_file.close()
+
 
         except Exception as e:
             raise
+
+
+    # after calling createInvertedIndex, the inverted index has now been sorted and can be written to file
+    def loadInvertedIndex(self, proc_doc_location):
+        if not proc_doc_location[len(proc_doc_location)-1] == '/':
+            proc_doc_location += "/"
+
+        if not os.path.exists(proc_doc_location):
+            print("The Input Directory Does Not Exist")
+            return
+
+        # This function is incomplete
